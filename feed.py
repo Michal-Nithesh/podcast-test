19f25771e6676bac870de6b842c3b57d64b0bc8f
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2024-08-13 09:32:02 +0000

    Completed itunes

--- feed.py
+++ feed.py
@@ -21,6 +21,26 @@ xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
 xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
 xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
 xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+xml_tree.SubElement(channel_element, 'link').text = link_prefix
+
+xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
+
+for item in yaml_data['item']:
+    item_element = xml_tree.SubElement(channel_element, 'item')
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(item_element, 'description').text = item['description']
+    xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
+    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+        'url': link_prefix + item['file'],
+        'type': 'audio/mpeg',
+        'length': item['length']
+    })
+
+
 
 output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('feed.xml', encoding='utf-8', xml_declaration=True)
+output_tree.write('podcast.xml', encoding='utf-8', xml_declaration=True)
